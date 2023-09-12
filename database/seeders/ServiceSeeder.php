dc0d486a7c3851a072d90b740074c0bfd4281378
AntonelloStefania <antonelloste94@gmail.com>
2023-09-12 10:38:52 +0200

    eliminate colonne ridondanti in services e photos

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServiceSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class ServiceSeeder extends Seeder
         $services = ['wi-fi', 'piscina','parcheggio privato', 'sauna', 'vista mare', 'portineria', 'servizio di trasporto', 'lavanderia', 'servizio in camera', 'sala lettura'];
         foreach($services as $service){
             $service_name = new Service();
-            $service_name->service = $service;
+            $service_name->service_1 = $service;
             $service_name->save();
         }
     }
