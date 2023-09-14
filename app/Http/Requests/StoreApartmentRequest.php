177782b1f19091998a4b83b28c55e66483871f15
GiadaTonioni <giada.tonioni@gmail.com>
2023-09-14 12:58:42 +0200

    validation e messages

--- app/Http/Requests/StoreApartmentRequest.php
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -24,7 +24,42 @@ class StoreApartmentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|max:50',
+            'address'=> 'required|max:255',
+            'lat'=> 'nullable|max:255',
+            'lon'=> 'nullable|max:255',
+            'cover'=> 'required|url:http,https',
+            'price'=> 'required|double(7,2)',
+            'visibility' => 'required|tinyint',
+            'description' => 'nullable',
+            'n_rooms' => 'required|tinyint',
+            'n_wc' => 'required|tinyint',
+            'mq' => 'required|tinyint',
+            'type_id'=> 'required|exists:types,id',
+        ];
+    }
+    public function messages(){
+        return[
+            'title.required'=>'il titolo è obbligatorio',
+            'title.max'=> 'il titolo deve essere di max:max caratteri',
+            'address.required' => 'un indirizzo è obbligatorio',
+            'address.max'=> 'indirizzo deve essere max:max caratteri',
+            'lat.max'=> 'la latitudine deve essere max:max caratteri',
+            'lon.max'=> 'la longitudine deve essere max:max caratteri',
+            'cover.required'=>'un immagine di copertina è obbligatoria',
+            'cover.url'=>'un immagine deve essere un url http o https',
+            'price.required'=>'il prezzo è obbligatorio',
+            'price.double'=>'il prezzo deve avere 7 cifre totali con 2 decimali',
+            'visibility.required'=>'la visibilità è obbligatoria',
+            'visibility.tinyint'=>'il numero deve essere 0 o 1',
+            'n_rooms.required'=>'il numero di stanze è obbligatorio',
+            'n_rooms.tinyint'=>'il numero di stanze è massimo 127',
+            'n_wc.required'=> 'il numero di bagni è obbligatorio',
+            'n_wc.tinyint'=>'il numero di wc è massimo 127',
+            'mq.required'=>'il numero di mq è obbligatorio',
+            'mq.tinyint'=>'il numero di mq è massimo 127',
+            'type_id.required'=>'è obbligatorio selezionare una tipologia',
+            'type_id.exists'=>'devi scegliere una tipologia esistente',
         ];
     }
 }
