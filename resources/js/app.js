f58e1c34a2877408bb586b089b5773d49d563ac5
alep-2000 <alessiopiras056@gmail.com>
2023-09-20 10:21:05 +0200

    Modify

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,40 +3,40 @@ import '~resources/scss/app.scss';
 import Axios from 'axios';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
-    '../img/**'
+  '../img/**'
 ]);
 
 //POPOVER INFO DESCRIZIONE APPARTAMENTO IN CREATE
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 })
 
 
 //SELEZIONE TIPOLOGIA APPARTAMENTO IN CREATE (RADIO)
 document.addEventListener('DOMContentLoaded', function () {
-    const radioButtons = document.querySelectorAll('input[name="type_id"]');
-    const resultDiv = document.getElementById('result');
+  const radioButtons = document.querySelectorAll('input[name="type_id"]');
+  const resultDiv = document.getElementById('result');
 
-    radioButtons.forEach(function (radioButton) {
-        radioButton.addEventListener('change', function () {
-            // Recupera l'ID dell'elemento selezionato
-            const selectedID = radioButton.value;
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+      // Recupera l'ID dell'elemento selezionato
+      const selectedID = radioButton.value;
 
-            // Rimuovi il background-color da tutti gli elementi
-            radioButtons.forEach(function (rb) {
-                const label = document.querySelector(`label[for="type-id-${rb.value}"]`);
-                label.style.backgroundColor = 'transparent';
-            });
+      // Rimuovi il background-color da tutti gli elementi
+      radioButtons.forEach(function (rb) {
+        const label = document.querySelector(`label[for="type-id-${rb.value}"]`);
+        label.style.backgroundColor = 'transparent';
+      });
 
-            if (radioButton.checked) {
-                // Imposta il background-color dell'elemento selezionato
-                const selectedLabel = document.querySelector(`label[for="type-id-${selectedID}"]`);
-                selectedLabel.style.backgroundColor = '#C0C9E1';
-                selectedLabel.style.borderRadius = '1rem';
-            }
-        });
+      if (radioButton.checked) {
+        // Imposta il background-color dell'elemento selezionato
+        const selectedLabel = document.querySelector(`label[for="type-id-${selectedID}"]`);
+        selectedLabel.style.backgroundColor = '#C0C9E1';
+        selectedLabel.style.borderRadius = '1rem';
+      }
     });
+  });
 });
 
 
@@ -52,59 +52,59 @@ let lon = '';
 let lat = '';
 let addressInput = document.getElementById('address');
 
-addressInput.addEventListener("keyup", function() {
-    // Ottieni il valore dell'input
-    let query = addressInput.value;
+addressInput.addEventListener("keyup", function () {
+  // Ottieni il valore dell'input
+  let query = addressInput.value;
 
-    console.log(query);
-    // Se il valore è vuoto, svuota la lista dei suggerimenti e nascondi i dettagli dell'indirizzo
-    if (query === "") {
-      suggestionsList.innerHTML = "";
-      return;
-    }
+  console.log(query);
+  // Se il valore è vuoto, svuota la lista dei suggerimenti e nascondi i dettagli dell'indirizzo
+  if (query === "") {
+    suggestionsList.innerHTML = "";
+    return;
+  }
 
-    let url = "https://api.tomtom.com/search/2/search/" + query + ".json?key=" + 'NvRVuGxMpACPuu2WUR93HOEvbVfg2g9A' + "&typeahead=true&limit=";
+  let url = "https://api.tomtom.com/search/2/search/" + query + ".json?key=" + 'NvRVuGxMpACPuu2WUR93HOEvbVfg2g9A' + "&typeahead=true&limit=";
 
-    // Invia la richiesta GET usando la funzione fetch
-    fetch(url)
-    .then(function(response) {
-    // Se la risposta è valida, restituisci l'oggetto JSON
-    if (response.ok) {
+  // Invia la richiesta GET usando la funzione fetch
+  fetch(url)
+    .then(function (response) {
+      // Se la risposta è valida, restituisci l'oggetto JSON
+      if (response.ok) {
         return response.json();
-    }
-    // Altrimenti, mostra un messaggio di errore
-    else {
+      }
+      // Altrimenti, mostra un messaggio di errore
+      else {
         throw new Error("Si è verificato un errore nella richiesta: " + response.status);
-    }
+      }
     })
-    .then(function(data) {
-    // Se l'oggetto JSON contiene dei risultati, popola la lista dei suggerimenti
-    if (data.results.length > 0) {
+    .then(function (data) {
+      // Se l'oggetto JSON contiene dei risultati, popola la lista dei suggerimenti
+      if (data.results.length > 0) {
         populateSuggestionsList(data.results);
-    }
-    // Altrimenti, svuota la lista dei suggerimenti e nascondi i dettagli dell'indirizzo
-    else {
+      }
+      // Altrimenti, svuota la lista dei suggerimenti e nascondi i dettagli dell'indirizzo
+      else {
         suggestionsList.innerHTML = "";
-    }
+      }
     })
-    .catch(function(error) {
-    // Mostra il messaggio di errore nella console
-    console.error(error);
+    .catch(function (error) {
+      // Mostra il messaggio di errore nella console
+      console.error(error);
     });
 });
 // Funzione che popola la lista dei suggerimenti con gli elementi li
 function populateSuggestionsList(results) {
-    // Svuota la lista dei suggerimenti
-    suggestionsList.innerHTML = "";
-    // Crea un ciclo for per ogni risultato
-    for (let i = 0; i < results.length; i++) {
-        // Crea un elemento li
-        let option = document.createElement("option");
-        // Imposta il testo dell'elemento option con il valore dell'attributo address.freeformAddress del risultato
-        option.textContent = results[i].address.freeformAddress;
-        // Aggiunge i risultati all'interno del datalist
-        suggestionsList.append(option)
-    }
+  // Svuota la lista dei suggerimenti
+  suggestionsList.innerHTML = "";
+  // Crea un ciclo for per ogni risultato
+  for (let i = 0; i < results.length; i++) {
+    // Crea un elemento li
+    let option = document.createElement("option");
+    // Imposta il testo dell'elemento option con il valore dell'attributo address.freeformAddress del risultato
+    option.textContent = results[i].address.freeformAddress;
+    // Aggiunge i risultati all'interno del datalist
+    suggestionsList.append(option)
+  }
 };
 
 // submitForm.addEventListener('click', (event) => {
@@ -121,7 +121,7 @@ function populateSuggestionsList(results) {
 //             // addressValue === data.results[0].address.freeformAddress
 //             if(addressValue === data.results[0].address.freeformAddress){
 //                 // salva in una variabile la risposta dell'API, in JSON
-                
+
 //                 lon = data.results[0].position.lon
 //                 lat = data.results[0].position.lat
 
@@ -143,54 +143,54 @@ function populateSuggestionsList(results) {
 // })
 
 submitForm.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const addressValue = document.getElementById('address').value;
-    console.log(addressValue);
+  const addressValue = document.getElementById('address').value;
+  console.log(addressValue);
 
-    let options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: null // non serve un corpo per una richiesta GET
-    };
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: null // non serve un corpo per una richiesta GET
+  };
 
-    fetch('https://api.tomtom.com/search/2/geocode/' + addressValue + '.json?key=NvRVuGxMpACPuu2WUR93HOEvbVfg2g9A', options)
-      .then(response => {
-        // controllare se la risposta è valida
-        if (response.ok) {
-          // convertire la risposta in JSON
-          return response.json();
-        } else {
-          // lanciare un errore se la risposta non è valida
-          throw new Error("Errore nella richiesta: " + response.status);
-        }
-      })
-      .then(data => {
-        // gestire i dati ricevuti
-        // il codice qui è lo stesso della funzione onload di XMLHttpRequest
-        if(addressValue === data.results[0].address.freeformAddress){
-          // salva in una variabile la risposta dell'API, in JSON
-                
-          lon = data.results[0].position.lon
-          lat = data.results[0].position.lat
+  fetch('https://api.tomtom.com/search/2/geocode/' + addressValue + '.json?key=NvRVuGxMpACPuu2WUR93HOEvbVfg2g9A', options)
+    .then(response => {
+      // controllare se la risposta è valida
+      if (response.ok) {
+        // convertire la risposta in JSON
+        return response.json();
+      } else {
+        // lanciare un errore se la risposta non è valida
+        throw new Error("Errore nella richiesta: " + response.status);
+      }
+    })
+    .then(data => {
+      // gestire i dati ricevuti
+      // il codice qui è lo stesso della funzione onload di XMLHttpRequest
+      if (addressValue === data.results[0].address.freeformAddress) {
+        // salva in una variabile la risposta dell'API, in JSON
 
-          document.getElementById('lon').value = lon;
-          document.getElementById('lat').value = lat;
-          console.log(document.getElementById('lon').value)
-          console.log(document.getElementById('lat').value)
-          const form = document.getElementById('form');
-          form.submit();
-        } else {
-          console.log('Indirizzo non valido')
-          document.getElementById('addressError').innerText = 'Indirizzo non valido'
-        }
-      })
-      .catch(error => {
-        // gestire gli errori
-        console.log("Errore: " + error.message);
-      });
+        lon = data.results[0].position.lon
+        lat = data.results[0].position.lat
+
+        document.getElementById('lon').value = lon;
+        document.getElementById('lat').value = lat;
+        console.log(document.getElementById('lon').value)
+        console.log(document.getElementById('lat').value)
+        const form = document.getElementById('form');
+        form.submit();
+      } else {
+        console.log('Indirizzo non valido')
+        document.getElementById('addressError').innerText = 'Indirizzo non valido'
+      }
+    })
+    .catch(error => {
+      // gestire gli errori
+      console.log("Errore: " + error.message);
+    });
 });
 
 
